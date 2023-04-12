45a0705750de4ba2d2cb4f3ad3f48f62c8743b67
Sunny <aaryan.sharmaa04@gmail.com>
2023-04-12 15:12:29 +0530

    First push

---
+++ gitpush.c
@@ -0,0 +1,12 @@
+// print 1-n using loops.
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number");
+    scanf("%d", &n);
+    for(int i =1 ; i<=n; i++)
+    printf("%d \n", i);
+    
+    return 0;
+}
